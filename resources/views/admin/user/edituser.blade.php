d754dc59fd1cb093a6f1b803b303fdfd6f025963
Thuan <thuan.nv.191297@gmail.com>
2019-01-14 13:44:28 +0700

    fix fe admin

--- resources/views/admin/user/edituser.blade.php
+++ resources/views/admin/user/edituser.blade.php
@@ -38,7 +38,7 @@
               <input type="hidden" name="_token" value="{{ csrf_token() }}">
               <div class="edit-tk__content--row">
                 <div class="text"> <span>
-                     Full name (<span class="required">*</span>):</span></div>
+                     Full name :</span></div>
                 <div class="content">
                   <input class="border--base padding--base" id="fullname" type="text" value="{{ $errors->has('fullname') ? old('fullname') : $user->fullname }}" name="fullname" required maxlength="30" pattern="^[A-Za-z -+()]*$" />
                     <div class="error-fullname">
@@ -53,7 +53,7 @@
                 <div class="clear-fix"></div>
                 <div class="edit-tk__content--row">
                 <div class="text"><span>
-                     Email (<span class="required">*</span>):</span></div>
+                     Email :</span></div>
                 <div class="content">
                   <input class="border--base padding--base" id="email" type="text" value="{{$errors->has('email') ? old('email') : $user->email}}" name="email" pattern="[A-Za-z0-9._%+-]{3,}@[a-zA-Z]{3,}([.]{1}[a-zA-Z]{2,}|[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,})" required maxlength="50" />
                   <div class="error-email">
@@ -68,7 +68,7 @@
               </div>
               <div class="edit-tk__content--row">
                 <div class="text"><span>
-                     Username (<span class="required">*</span>):</span></div>
+                     Username :</span></div>
                 <div class="content">
                   <input class="border--base padding--base" id="username" type="text" value="{{$errors->has('username') ? old('username') : $user->username}}" name="username" required minlength="3" maxlength="50" />
                   <div class="error-username">
@@ -83,7 +83,7 @@
               </div>
               <div class="edit-tk__content--row">
                 <div class="text"><span>
-                      Password (<span class="required">*</span>):</span></div>
+                      Password :</span></div>
                 <div class="content">
                   <input class="border--base padding--base" id="password" type="text" name="password" value="" minlength="6" maxlength="18" />
                   <div class="error-password">
@@ -98,7 +98,7 @@
               </div>
               <div class="edit-tk__content--row">
                 <div class="text"><span>
-                     Confirm password(<span class="required">*</span>):</span></div>
+                     Confirm password:</span></div>
                 <div class="content">
                   <input class="border--base padding--base" id="confirm" type="text" name="confirm" minlength="6" maxlength="18"/>
                   <div class="error-confirm">
@@ -113,7 +113,7 @@
               </div>
               <div class="edit-tk__content--row">
                 <div class="text"><span>
-                     Gender (<span class="required">*</span>):</span></div>
+                     Gender :</span></div>
                 @if($user->gender == 1)
                 <div class="content">
                   <label for="sex-male">Male</label>
@@ -127,7 +127,7 @@
               </div>
               <div class="edit-tk__content--row">
                 <div class="text"><span>
-                     Phone (<span class="required">*</span>):</span></div>
+                     Phone :</span></div>
                 <div class="content">
                   <input class="border--base padding--base" id="phone" type="text" value="{{$errors->has('phone') ? old('phone') : $user->phone}}" name="phone" minlength="10" maxlength="15" pattern="^[0-9-+()]*$" required />
                   <div class="error-phone">
@@ -142,7 +142,7 @@
               </div>
               <div class="edit-tk__content--row">
                 <div class="text"><span>
-                     Date of birth (<span class="required">*</span>):</span></div>
+                     Date of birth :</span></div>
                 <div class="content date--wrap myDate"><i class="far fa-calendar-alt"></i>
                   <input class="myDatePicker border--base padding--base" id="birthday" type="text" value="{{$errors->has('birthday') ? old('birthday') : $user->birthday}}" name="birthday" required />
                   <div class="error-birthday">
@@ -157,7 +157,7 @@
               </div>
               <div class="edit-tk__content--row">
                 <div class="text"><span>
-                     Address (<span class="required">*</span>):</span></div>
+                     Address :</span></div>
                 <div class="content">
                   <input class="account-address border--base padding--base" id="address" type="text" name="address" value="{{$errors->has('address') ? old('address') : $user->address}}" required maxlength="190" />
                   <div class="error-address">
@@ -172,7 +172,7 @@
               </div>
               <div class="edit-tk__content--row">
                 <div class="text"><span>
-                     Status (<span class="required">*</span>):</span></div>
+                     Status :</span></div>
                 <div class="content">
                   <select class="account-position border--base padding--base" id="status" name="status">
                     @if(old('status'))
@@ -197,7 +197,7 @@
               </div>
               <div class="edit-tk__content--row">
                 <div class="text"><span>
-                     Position (<span class="required">*</span>):</span></div>
+                     Position :</span></div>
                 <div class="content">
                   <select class="account-position border--base padding--base" id="position" name="position">
                     @if(old('position'))
